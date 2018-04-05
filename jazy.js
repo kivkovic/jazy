24bb98bf8994c5d6e025294e2ff79a63496ebda2
Kresimir Ivkovic <kivkovic@gmail.com>
2018-04-05 22:25:41 +0200

    Changed interface

--- jazy.js
+++ jazy.js
@@ -1,41 +1,43 @@
-function jazy(path, options) {
-    options = options || {};
+window.jazy = {
+    load: function(path, options) {
+        options = options || {};
 
-    if (!window['@@jazy_' + path]) {
-        var caught = function() {},
-            callbacks = [],
-            module = document.createElement(options.tag || 'script');
+        if (!window['@@jazy_' + path]) {
+            var caught = function() {},
+                callbacks = [],
+                module = document.createElement(options.tag || 'script');
 
-        module.async = options.async != null ? options.async : true;
+            module.async = options.async != null ? options.async : true;
 
-        jazy.then = function(callback) {
-            callbacks.push(callback);
-            return this;
-        }
+            jazy.then = function(callback) {
+                callbacks.push(callback);
+                return this;
+            }
 
-        jazy.catch = function(callback) {
-            caught = callback;
-        }
+            jazy.catch = function(callback) {
+                caught = callback;
+            }
 
-        module.onerror = function(e) { caught(); }
+            module.onerror = function(e) { caught(); }
 
-        module.onreadystatechange = module.onload = function() {
-            window['@@jazy_' + path] = 1;
-            while (callbacks.length) {
-                (callbacks.shift())();
+            module.onreadystatechange = module.onload = function() {
+                window['@@jazy_' + path] = 1;
+                while (callbacks.length) {
+                    (callbacks.shift())();
+                }
+            }
+
+            module[options.srcKey || 'src'] = path;
+
+            document.head.appendChild(module);
+
+        } else {
+            jazy.then = function(callback) {
+                callback();
+                return this;
             }
         }
 
-        module[options.srcKey || 'src'] = path;
-
-        document.head.appendChild(module);
-
-    } else {
-        jazy.then = function(callback) {
-            callback();
-            return this;
-        }
+        return jazy;
     }
-
-    return jazy;
 }
